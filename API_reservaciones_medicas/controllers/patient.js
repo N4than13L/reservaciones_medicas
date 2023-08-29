83d890c0803439f3899badd41e17698692e466e4
Nathaniel <natharevolution@gmail.com>
2023-08-29 16:51:44 -0400

    agregando funcion para agregar pacientes

--- API_reservaciones_medicas/controllers/patient.js
+++ API_reservaciones_medicas/controllers/patient.js
@@ -12,6 +12,35 @@ const pruebaPatient = (req, res) => {
   });
 };
 
+const guardarPaciente = (req, res) => {
+  // recoger los datos de la peticion.
+  let params = req.body;
+
+  // comprobar que me lleguen los datos (+ validacion).
+  if (!params.name || !params.surname || !params.nick) {
+    return res.status(400).json({
+      status: "error",
+      message: "Faltan datos por enviar.",
+    });
+  }
+
+  // crear objeto del usuario.
+  let paceinte_a_guardar = new Patient(params);
+
+  // guardar usuarios.
+  paceinte_a_guardar.save().then((paciente_guardado) => {
+    // devolver datos de la respuesta.
+    if (paciente_guardado) {
+      return res.status(200).json({
+        status: "success",
+        message: "usuario pacinte correctamente",
+        usuario: paciente_guardado,
+      });
+    }
+  });
+};
+
 module.exports = {
   pruebaPatient,
+  guardarPaciente,
 };
--- API_reservaciones_medicas/routes/patient.js
+++ API_reservaciones_medicas/routes/patient.js
@@ -2,9 +2,15 @@
 const express = require("express");
 const router = express.Router();
 const patientsController = require("../controllers/patient");
+const check = require("../middlewares/auth");
 
 // definir rutas
 router.get("/paciente-test", patientsController.pruebaPatient);
+router.post(
+  "/guardar-paciente",
+  check.auth,
+  patientsController.guardarPaciente
+);
 
 // exportar
 module.exports = router;
