c5f31b280341b9b8fb892b3b8de9367a012c4d4e
nathaniel <natharevolution@gmail.com>
2023-09-01 22:29:06 -0400

    quitando comentarios de los componentes

--- reservaciones_medicas_app/src/views/Login.js
+++ reservaciones_medicas_app/src/views/Login.js
@@ -1,21 +1,3 @@
-/*!
-
-=========================================================
-* Paper Dashboard React - v1.3.2
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/paper-dashboard-react
-* Copyright 2023 Creative Tim (https://www.creative-tim.com)
-
-* Licensed under MIT (https://github.com/creativetimofficial/paper-dashboard-react/blob/main/LICENSE.md)
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/
 import { Global } from "helpers/global";
 // import UseAuth from "hooks/useAuth";
 import { UseForm } from "hooks/useForm";
